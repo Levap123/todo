9f6084fe5cba748c11b0295352ee926e3a2935b1
Levap123 <96424716+Levap123@users.noreply.github.com>
2022-06-01 09:07:38 +0600

    123

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/Levap123/toDo.git"
-	"github.com/Levap123/toDo.git/package/handler"
+	"github.com/Levap123/todo.git"
+	"github.com/Levap123/todo.git/package/handler"
 	"log"
 )
 
 func main() {
 	handler := new(handler.Handler)
 	server := new(todo.Server)
-	err := server.Run("4444", handler.InitRoutes())
+	err := server.Run(":4444", handler.InitRoutes())
 	if err != nil {
 		log.Fatalln(err)
 	}
